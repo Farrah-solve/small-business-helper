96867c5c1d137e2a967385755f6bfa70bce8e94f
Farrah-solve <68975737+Farrah-solve@users.noreply.github.com>
2021-02-02 23:35:01 -0500

    added mainpage

---
+++ client/src/pages/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const MainPage = () => {
+
+    return (
+        <div>
+            <h3>Welcome to Small Business Helper</h3>
+            <small>Login Page</small>
+        </div>
+    );
+};
+
+export default MainPage;
