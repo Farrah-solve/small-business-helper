ca6da8445427efccf1613acac8ad00629e0a17c0
Farrah-solve <68975737+Farrah-solve@users.noreply.github.com>
2021-02-02 23:44:30 -0500

    begun routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
+import {
+   BrowserRouter as Router, 
+   Route, 
+   Switch, 
+   Link, 
+   Redirect 
+  } from 'react-router-dom';
+
+import MainPage from './pages';
+
 class App extends Component {
   render() {
-    return <div className="App">Welcome to
-    <p>Small Business Helper</p>
-    </div>;
+    return (
+    <Router>
+      <Route path="/" component={MainPage} />
+    </Router>
+    );
   }
 }
 
